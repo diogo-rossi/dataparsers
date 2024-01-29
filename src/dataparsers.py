abce9740e4a28a33fd099b363507f94e9c74e20c
Diogo Folador <folador.diogo@gmail.com>
2024-01-29 00:24:03 -0300

    🎉 new: add `dataparsers` module

---
+++ src/dataparsers.py
@@ -0,0 +1,74 @@
+# %% ############################################### data parsers region ###############################################
+
+
+from dataclasses import dataclass, field, fields
+from argparse import ArgumentParser, _MutuallyExclusiveGroup
+from typing import Any, TypeVar, Sequence
+
+SPECIALATTRIBUTE = "__data_parsers_params__"
+Class = TypeVar("Class", covariant=True)
+
+
+def arg(*name_or_flags: str, default=None, mutually_exclusive_group: str | int | None = None, **kwargs) -> Any:
+    # Error if `name_or_flags` is given without flags
+    if name_or_flags and not all(n.startswith("-") for n in name_or_flags):
+        raise ValueError(
+            "The argument `name_or_flags` should be passed to function `arg` only if it is a flag (starts with `-`)"
+        )
+
+    if "dest" in kwargs:
+        raise ValueError("The argument `dest` is not necessary")
+
+    arg_dict = dict(name_or_flags=name_or_flags, mutually_exclusive_group=mutually_exclusive_group, **kwargs)
+
+    # remove dict nones
+    arg_dict = {key: value for key, value in arg_dict.items() if value is not None}
+
+    return field(default=default, metadata=arg_dict)
+
+
+def dataparser(*args, required_mutually_exclusive_groups: dict[str | int, bool] | None = None, **kwargs):
+    if required_mutually_exclusive_groups is None:
+        required_mutually_exclusive_groups = {}
+
+    def wrap(cls: type[Class]) -> type[Class]:
+        cls = dataclass(cls)
+        setattr(cls, SPECIALATTRIBUTE, (args, kwargs, required_mutually_exclusive_groups))
+        return cls
+
+    return wrap
+
+
+def parse(cls: type[Class], args: Sequence[str] | None = None, *, parser: ArgumentParser | None = None) -> Class:
+    argparser_args, argparser_kwargs, required_groups = getattr(cls, SPECIALATTRIBUTE, ((), {}, {}))
+
+    if parser is None:
+        parser = ArgumentParser(*argparser_args, **argparser_kwargs)
+
+    groups: dict[str | int, _MutuallyExclusiveGroup] = {}
+
+    for arg in fields(cls):  # type: ignore
+        # Transform to dict because MappingProxyType is not subscriptable
+        arg_metadata = dict(arg.metadata)
+
+        # Get name_or_flags argument
+        if not arg_metadata.get("name_or_flags"):  # no flagged arg
+            arg_metadata["name_or_flags"] = (arg.name,)
+        else:  # flagged arg
+            arg_metadata["dest"] = arg.name
+        name_or_flags = arg_metadata.pop("name_or_flags")
+
+        group_id: str | int | None = arg_metadata.pop("mutually_exclusive_group", None)
+        if group_id is not None:
+            if group_id not in groups:
+                groups[group_id] = parser.add_mutually_exclusive_group(
+                    required=required_groups.get(group_id, False),
+                )
+            groups[group_id].add_argument(*name_or_flags, **arg_metadata)
+        else:
+            parser.add_argument(*name_or_flags, **arg_metadata)
+
+    return parser.parse_args(args, namespace=cls())
+
+
+# %% ###################################################################################################################
