b9ed9d670cda2621a5f0287f25edb23a5658d8df
Diogo Rossi <rossi.diogo@gmail.com>
2024-02-14 10:11:55 -0300

    ✅ test: update examples

--- examples/example.py
+++ examples/example.py
@@ -11,58 +11,68 @@ sys.path.insert(0, str(SRC_DIR))
 # %% Imports
 
 from dataclasses import dataclass
-from dataparsers import arg, make_parser, dataparser
+from dataparsers import arg, make_parser, dataparser, parse
 
 # %% Example 1
 
 
+# prog.py
 @dataclass
-class Arg1:
-    foo: str = arg(default=42)
-    bar: int = arg()
+class Args1:
+    foo: str
+    bar: int = 42
 
 
-parser = make_parser(Arg1)
-parser.print_help()
+args = parse(Args1)
+print("Printing `args`:")
+print(args)
 
 
 # %% Example 2
 
 
 @dataclass
-class Arg2:
-    foo: str = arg("-f")
-    bar: str = arg("-b")
+class Args2:
+    foo: str = arg(help="foo help")
+    bar: int = arg(default=42, help="bar help")
 
 
-parser = make_parser(Arg2)
-parser.print_help()
+args = parse(Args2)
 
 
 # %% Example 3
 
 
 @dataclass
-class Arg3:
-    foo: str = arg(default=42)
-    bar: int = arg()
+class Args3:
+    foo: str = arg(help="foo help")
+    bar: int = arg("-b", default=42, help="bar help")
 
 
-parser = make_parser(Arg3)
-parser.print_help()
+args = parse(Args3)
 
 
 # %% Example 4
 
 
 @dataclass
-class Arg4:
-    foo: str = arg(group_title="group", help="foo help", make_flag=True)
-    bar: str = arg(group_title="group", help="bar help")
+class Args4:
+    foo: str = arg(help="foo help")
+    bar: int = arg("-b", "--bar", default=42, nargs="?", help="bar help")
+
+args = parse(Args4)
 
 
-parser = make_parser(Arg4)
-parser.print_help()
+
+# %% Example 5
+
+
+@dataclass
+class Args5:
+    foo: str = arg(help="foo help")
+    bar: int = arg("-b", default=42, help="bar help", make_flag=False)
+
+args = parse(Args5)
 
 
 # %% Example 5
@@ -76,3 +86,16 @@ class Arg5:
 
 parser = make_parser(Arg5)
 parser.print_help()
+
+
+# %% Example 6
+
+
+@dataclass
+class Args:
+    foo: bool = arg(action="store_true", mutually_exclusive_group_id=1)
+    bar: bool = arg(action="store_false", mutually_exclusive_group_id=1)
+
+
+parse(Args, "--foo", "--bar")
+# parser.print_help()
