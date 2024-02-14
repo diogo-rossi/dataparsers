3da1310379b8753020f1d60b3b5ed20e76cf42f6
Diogo Rossi <rossi.diogo@gmail.com>
2024-02-14 10:11:54 -0300

    ✨ feat: support for simple groups

--- src/dataparsers.py
+++ src/dataparsers.py
@@ -1,7 +1,7 @@
 # %% ################################################# dataparsers region ######################################################
 import os, textwrap
 from dataclasses import dataclass, field, fields
-from argparse import ArgumentParser, _MutuallyExclusiveGroup
+from argparse import ArgumentParser, _MutuallyExclusiveGroup, _ArgumentGroup
 from typing import Any, TypeVar, Sequence, Callable, overload
 
 Class = TypeVar("Class", covariant=True)
@@ -9,7 +9,7 @@ Class = TypeVar("Class", covariant=True)
 
 def arg(
     *name_or_flags: str,
-    default=None,
+    group: str | int | None = None,
     mutually_exclusive_group: str | int | None = None,
     make_flag: bool | None = None,
     **kwargs,
@@ -28,20 +28,29 @@ def arg(
     if "dest" in kwargs:
         raise ValueError("The argument `dest` is not necessary")
 
+    if group is not None and mutually_exclusive_group is not None:
+        raise ValueError("Can't pass both `group` and `mutually_exclusive_group`")
+
+    default = kwargs.pop("default", None)
+
+    if not is_flag and mutually_exclusive_group is not None:
+        make_flag = True
+
     make_flag = bool(make_flag)
     is_flag = is_flag or make_flag
 
-    arg_dict = dict(
+    metadict = dict(
         name_or_flags=name_or_flags,
+        group=group,
         mutually_exclusive_group=mutually_exclusive_group,
         is_flag=is_flag,
         make_flag=make_flag,
         **kwargs,
     )
 
-    arg_dict = {key: value for key, value in arg_dict.items() if value is not None}
+    metadict = {key: value for key, value in metadict.items() if value is not None}
 
-    return field(default=default, metadata=arg_dict)
+    return field(default=default, metadata=metadict)
 
 
 @overload
@@ -50,32 +59,46 @@ def dataparser(cls: type[Class]) -> type[Class]: ...
 
 @overload
 def dataparser(
-    *, required_mutually_exclusive_groups: dict[str | int, bool] | None = None, default_store_bool: bool = True, **kwargs
+    *,
+    groups: dict[str | int, bool] | None = None,
+    required_mutually_exclusive_groups: dict[str | int, bool] | None = None,
+    default_store_bool: bool = True,
+    **kwargs,
 ) -> Callable[[type[Class]], type[Class]]: ...
 
 
 def dataparser(
-    cls, *, required_mutually_exclusive_groups=None, default_store_bool=True, **kwargs
+    cls: type[Class] | None = None,
+    *,
+    groups: dict[str | int, bool] | None = None,
+    required_mutually_exclusive_groups=None,
+    default_store_bool=True,
+    **kwargs,
 ) -> type[Class] | Callable[[type[Class]], type[Class]]:
     if cls is not None:
         return dataclass(cls)
+
+    if groups is None:
+        groups = {}
 
     if required_mutually_exclusive_groups is None:
         required_mutually_exclusive_groups = {}
 
     def wrap(cls: type[Class]) -> type[Class]:
         cls = dataclass(cls)
-        setattr(cls, "__dataparsers_params__", (kwargs, required_mutually_exclusive_groups, default_store_bool))
+        setattr(cls, "__dataparsers_params__", (kwargs, groups, required_mutually_exclusive_groups, default_store_bool))
         return cls
 
     return wrap
 
 
 def parse(cls: type[Class], args: Sequence[str] | None = None, *, parser: ArgumentParser | None = None) -> Class:
-    kwargs, required_groups, default_bool = getattr(cls, "__dataparsers_params__", ({}, {}, False))
-    groups: dict[str | int, _MutuallyExclusiveGroup] = {}
+    kwargs, groups, required_groups, default_bool = getattr(cls, "__dataparsers_params__", ({}, {}, {}, False))
     if parser is None:
         parser = ArgumentParser(**kwargs)
+
+    arg_groups: dict[str | int, _ArgumentGroup] = {}
+    mutually_exclusive_groups: dict[str | int, _MutuallyExclusiveGroup] = {}
 
     for arg in fields(cls):  # type: ignore
         arg_metadata = dict(arg.metadata)
@@ -102,13 +125,18 @@ def parse(cls: type[Class], args: Sequence[str] | None = None, *, parser: Argume
         if "action" not in arg_metadata and arg.type == bool:
             arg_metadata["action"] = "store_false" if arg.default else "store_true"
 
-        group_id: str | int | None = arg_metadata.pop("mutually_exclusive_group", None)
+        group_id: str | int | None = arg_metadata.pop("group", None)
+        exclusive_group_id: str | int | None = arg_metadata.pop("mutually_exclusive_group", None)
         if group_id is not None:
-            if group_id not in groups:
-                groups[group_id] = parser.add_mutually_exclusive_group(
-                    required=required_groups.get(group_id, False),
+            if group_id not in arg_groups:
+                arg_groups[group_id] = parser.add_argument_group(title=str(group_id), description=groups.get(group_id, None))
+            arg_groups[group_id].add_argument(*name_or_flags, default=arg.default, **arg_metadata)
+        elif exclusive_group_id is not None:
+            if exclusive_group_id not in mutually_exclusive_groups:
+                mutually_exclusive_groups[exclusive_group_id] = parser.add_mutually_exclusive_group(
+                    required=required_groups.get(exclusive_group_id, False),
                 )
-            groups[group_id].add_argument(*name_or_flags, default=arg.default, **arg_metadata)
+            mutually_exclusive_groups[exclusive_group_id].add_argument(*name_or_flags, default=arg.default, **arg_metadata)
         else:
             parser.add_argument(*name_or_flags, default=arg.default, **arg_metadata)
 
