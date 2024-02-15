93156baa7e3f1dcd5ec8efd7fb3cdb45477681bd
Diogo Rossi <rossi.diogo@gmail.com>
2024-02-14 23:00:31 -0300

    📄 docs: add custom css in addition to sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,6 +95,6 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# html_css_files = ["css/custom.css"]
+html_css_files = ["css/custom.css"]
 
 default_role = "code"
