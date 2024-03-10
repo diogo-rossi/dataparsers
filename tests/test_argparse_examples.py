980b36303e599ab911cd20433e5c6324ca1726e3
Diogo Folador <folador.diogo@gmail.com>
2024-03-10 14:45:51 -0300

    ✅ test: correct tests

---
+++ tests/test_argparse_examples.py
@@ -0,0 +1,74 @@
+import os
+import sys
+from pathlib import Path
+
+SRC_DIR = Path(__file__).parent.parent.resolve() / "src"
+sys.path.insert(0, str(SRC_DIR))
+
+from dataclasses import dataclass
+from dataparsers import parse, make_parser, dataparser
+from dataparsers import arg, arg_group, mutually_exclusive_group, subparsers_group, subparser, post_default
+from dataparsers import ArgumentGroup, MutuallyExclusiveGroup, SubparsersGroup, Subparse
+from resources import HelpDisplay, CapSys
+from typing import ClassVar, Any, TypeAlias
+
+
+def parse_args_without_sysexit(cls: type, args: list[str]) -> None:
+    try:
+        parse(cls, args)
+    except SystemExit:
+        pass
+
+
+def test_subcommands_01(capsys: CapSys):
+    @dataparser(prog="PROG")
+    @dataclass
+    class Args:
+        foo: bool = arg(help="foo help")
+        subparser_name: SubparsersGroup = subparsers_group(help="sub-command help")
+        a: ClassVar = subparser(help="a help")
+        bar: int = arg(help="bar help", subparser=a)
+        b: ClassVar = subparser(help="b help")
+        baz: str = arg(make_flag=True, choices="XYZ", help="baz help", subparser=b)
+
+    parse_args_without_sysexit(Args, ["-h"])
+    output_display = HelpDisplay(capsys.readouterr().out)
+    assert "PROG [-h]" in output_display.usage
+    assert "a" in output_display.positionals
+    assert "b" in output_display.positionals
+
+    parse_args_without_sysexit(Args, ["a", "-h"])
+    output_display = HelpDisplay(capsys.readouterr().out)
+    assert "PROG a [-h] bar" in output_display.usage
+    assert "bar" in output_display.positionals
+
+    parse_args_without_sysexit(Args, ["b", "-h"])
+    output_display = HelpDisplay(capsys.readouterr().out)
+    assert "PROG b [-h] [--baz {X,Y,Z}]" in output_display.usage
+    assert "--baz" in output_display.flags
+
+    args = parse(Args, ["a", "12"])
+    assert args.bar == 12
+    assert args.foo == False
+    assert args == Args(foo=False, subparser_name="a", bar=12, baz=None)
+
+    args = parse(Args, ["--foo", "b", "--baz", "Z"])
+    assert args.baz == "Z"
+    assert args.foo == True
+    assert args == Args(foo=True, subparser_name="b", bar=None, baz="Z")
+
+
+def test_subcommands_02(capsys: CapSys):
+    @dataclass
+    class Args:
+        subparser_name: SubparsersGroup = subparsers_group(
+            title="subcommands", description="valid subcommands", help="additional help"
+        )
+        foo: ClassVar = subparser()
+        bar: ClassVar = subparser()
+
+    make_parser(Args).print_help()
+    output_display = HelpDisplay(capsys.readouterr().out)
+    subcommand_section = [s.strip() for s in output_display.get_section("subcommands")]
+    assert "valid subcommands" in subcommand_section
+    # assert "additional help" in subcommand_section
--- tests/test_dataparsers.py
+++ tests/test_dataparsers.py
@@ -6,25 +6,10 @@ import sys
 # add `src` folder to the `sys.path`, relative to the CWD
 sys.path.insert(0, os.path.abspath("./src"))
 
-from pytest import fixture
 from dataclasses import dataclass, fields
-from argparse import ArgumentParser
 from dataparsers import arg, dataparser, parse, make_parser
-from typing import Protocol, Literal
-
-
-class OutErr(Protocol):
-    out: str
-    err: str
-
-
-class CapSys(Protocol):
-    def readouterr(self) -> OutErr: ...
-
-
-@fixture
-def parser():
-    return ArgumentParser()
+from dataparsers import ArgumentGroup, arg_group, MutuallyExclusiveGroup, mutually_exclusive_group
+from resources import HelpDisplay, CapSys
 
 
 def test_00_only_positionals(capsys: CapSys):
@@ -36,7 +21,7 @@ def test_00_only_positionals(capsys: CapSys):
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    positionals = HelpOutput(output).positionals
+    positionals = HelpDisplay(output).positionals
     assert all(name in positionals for name in [f.name for f in fields(Args)])
 
 
@@ -50,8 +35,9 @@ def test_01_with_one_flag(capsys: CapSys):
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    flags = HelpOutput(output).flags
+    flags = HelpDisplay(output).flags
     assert "--foo" in flags
+
 
 def test_02_with_defaults_and_helps(capsys: CapSys):
     @dataclass
@@ -62,11 +48,12 @@ def test_02_with_defaults_and_helps(capsys: CapSys):
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    output = HelpOutput(output)
+    output = HelpDisplay(output)
     positionals = output.positionals
     flags = output.flags
     assert "string" in positionals
     assert "--integer" in flags
+
 
 def test_03_automatic_make_flags(capsys: CapSys):
     @dataclass
@@ -77,79 +64,81 @@ def test_03_automatic_make_flags(capsys: CapSys):
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    flags = HelpOutput(output).flags
+    flags = HelpDisplay(output).flags
     assert all(name in flags for name in ["-s", "-i"])
     assert all(name in flags for name in ["--string", "--integer"])
 
-def test_3_with_one_group(capsys: CapSys):
+
+def test_04_with_one_group(capsys: CapSys):
     @dataclass
     class Args:
-        string: str = arg(group_title="group1")
-        integer: int = arg(group_title="group1")
+        MyGroup: ArgumentGroup = arg_group("group1")
+        string: str = arg(group=MyGroup)
+        integer: int = arg(group=MyGroup)
 
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    group = HelpOutput(output).group("group1")
+    group = HelpDisplay(output).group("group1")
     assert all(name in group for name in [f.name for f in fields(Args)])
 
 
-def test_with_one_group_with_one_flag(capsys: CapSys):
+def test_05_with_one_group_with_one_flag(capsys: CapSys):
     @dataclass
     class Args:
-        string: str = arg(group_title="group1")
-        integer: int = arg(group_title="group1", make_flag=True)
+        MyGroup: ArgumentGroup = arg_group("group1")
+        string: str = arg(group=MyGroup)
+        integer: int = arg(group=MyGroup, make_flag=True)
 
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    group = HelpOutput(output).group("group1")
+    group = HelpDisplay(output).group("group1")
     assert all(name in group for name in ["string", "--integer"])
 
 
-def test_with_multually_exclusive_groups(capsys: CapSys):
+def test_06_with_multually_exclusive_groups(capsys: CapSys):
     @dataclass
     class Args:
-        string: str = arg(mutually_exclusive_group_id=1)
-        integer: int = arg(mutually_exclusive_group_id=1)
+        string: str = arg(mutually_exclusive_group=1)
+        integer: int = arg(mutually_exclusive_group=1)
 
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    flags = HelpOutput(output).flags
+    flags = HelpDisplay(output).flags
     assert all(name in flags for name in ["--string", "--integer"])
 
 
-def test_with_required_multually_exclusive_groups_and_no_given_flags(capsys: CapSys):
-    @dataparser(required_mutually_exclusive_groups={1: True})
+def test_07_with_required_multually_exclusive_groups_and_no_given_flags(capsys: CapSys):
+    @dataparser
     class Args:
-        string: str = arg(mutually_exclusive_group_id=1)
-        integer: int = arg(mutually_exclusive_group_id=1)
+        MyGroup: MutuallyExclusiveGroup = mutually_exclusive_group(required=True)
+        string: str = arg(mutually_exclusive_group=MyGroup)
+        integer: int = arg(mutually_exclusive_group=MyGroup)
 
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    flags = HelpOutput(output).flags
+    flags = HelpDisplay(output).flags
     assert all(name in flags for name in ["--string", "--integer"])
 
 
-def test_with_required_multually_exclusive_groups_and_given_single_flags(capsys: CapSys):
-    @dataparser(required_mutually_exclusive_groups={1: True})
+def test_08_with_required_multually_exclusive_groups_and_given_single_flags(capsys: CapSys):
+    @dataparser
     class Args:
-        string: str = arg("-s", mutually_exclusive_group_id=1)
-        integer: int = arg("-i", mutually_exclusive_group_id=1)
+        MyGroup: MutuallyExclusiveGroup = mutually_exclusive_group(required=True)
+        string: str = arg("-s", mutually_exclusive_group=MyGroup)
+        integer: int = arg("-i", mutually_exclusive_group=MyGroup)
 
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    flags = HelpOutput(output).flags
+    flags = HelpDisplay(output).flags
     assert all(name in flags for name in ["--string", "--integer"])
 
 
-
-
-
-def test_forced_make_flags(capsys: CapSys):
+def test_09_forced_make_flags(capsys: CapSys):
     @dataclass
     class Args:
         string: str = arg("-s", "--str", make_flag=True)
@@ -158,63 +147,5 @@ def test_forced_make_flags(capsys: CapSys):
     parser = make_parser(Args)
     parser.print_help()
     output = capsys.readouterr().out
-    flags = HelpOutput(output).flags
+    flags = HelpDisplay(output).flags
     assert all(name in flags for name in ["--string", "--integer"])
-
-
-
-
-
-# %% Help output handler
-
-
-@dataclass
-class HelpOutput:
-    help_text: str
-
-    def __post_init__(self):
-        self.text_lines = self.help_text.splitlines()
-        self.usage = self.get_usage()
-        self.positionals = self.get_positionals()
-        self.flags = self.get_flags()
-
-    def get_usage(self) -> str:
-        try:
-            return [line for line in self.text_lines if line.startswith("usage:")][0]
-        except IndexError:
-            return ""
-
-    def get_section(self, title: str) -> list[str]:
-        try:
-            start = self.text_lines.index([line for line in self.text_lines if line.strip().startswith(title)][0]) + 1
-            lines = []
-            for line in self.text_lines[start:]:
-                if not (line.startswith(" ") or line == ""):
-                    break
-                lines.append(line)
-            return lines
-        except IndexError:
-            return []
-
-    def get_positionals(self) -> list[str]:
-        return [
-            arg.strip().split()[0].strip()
-            for arg in self.get_section("positional arguments:")
-            if arg and not arg.startswith(" " * 10)
-        ]
-
-    def get_flags(self) -> list[str]:
-        flags = []
-        for line in self.get_section("options:"):
-            if line:
-                for part in line.split(","):
-                    flags.append(part.strip().split()[0])
-        return flags
-
-    def group(self, group_id) -> list[str]:
-        args = []
-        for line in self.get_section(f"{group_id}:"):
-            if line and not line.startswith(" " * 10):
-                for part in line.split(","):
-                    args.append(part.strip().split()[0])
-        return args
