8942a4a4810eaf2f6839a08e21b7eed7ba64281c
Diogo Rossi <rossi.diogo@gmail.com>
2024-06-30 14:28:21 -0300

    💄 style: format imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
 import shutil
+import sys
 
 # Add source to path
 sys.path.insert(0, os.path.abspath("."))
